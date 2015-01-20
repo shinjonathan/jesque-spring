0b50dc7bf7e614e91ac1ea8ad3615bfb3427548e
lariverosc <lariverosc@gmail.com>
2015-01-20 09:41:30 -0500

    Update to jesque 1.3.1 and next development version

--- src/main/java/net/lariverosc/jesquespring/SpringWorker.java
+++ src/main/java/net/lariverosc/jesquespring/SpringWorker.java
@@ -13,14 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.lariverosc.jesquespring;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicBoolean;
 import net.greghaines.jesque.Config;
 import net.greghaines.jesque.Job;
 import static net.greghaines.jesque.worker.WorkerEvent.JOB_PROCESS;
+import static net.greghaines.jesque.utils.ResqueConstants.WORKER;
 import net.greghaines.jesque.worker.WorkerImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,71 +35,89 @@ import org.springframework.context.ApplicationContextAware;
  */
 public class SpringWorker extends WorkerImpl implements ApplicationContextAware {
 
-	private Logger logger = LoggerFactory.getLogger(SpringWorker.class);
-	private ApplicationContext applicationContext;
+    private Logger logger = LoggerFactory.getLogger(SpringWorker.class);
 
-	/**
-	 *
-	 * @param config used to create a connection to Redis
-	 * @param queues the list of queues to poll
-	 */
-	public SpringWorker(final Config config, final Collection<String> queues) {
-		super(config, queues, Collections.EMPTY_MAP);
-	}
+    private ApplicationContext applicationContext;
 
-	@Override
-	protected void process(final Job job, final String curQueue) {
-		logger.info("Process new Job from queue {}", curQueue);
-		try {
-			Runnable runnableJob = null;
-			if (applicationContext.containsBeanDefinition(job.getClassName())) {//Lookup by bean Id
-				runnableJob = (Runnable) applicationContext.getBean(job.getClassName(), job.getArgs());
-			} else {
-				try {
-					Class clazz = Class.forName(job.getClassName());//Lookup by Class type
-					String[] beanNames = applicationContext.getBeanNamesForType(clazz, true, false);
-					if (applicationContext.containsBeanDefinition(job.getClassName())) {
-						runnableJob = (Runnable) applicationContext.getBean(beanNames[0], job.getArgs());
-					} else {
-						if (beanNames != null && beanNames.length == 1) {
-							runnableJob = (Runnable) applicationContext.getBean(beanNames[0], job.getArgs());
-						}
-					}
-				} catch (ClassNotFoundException cnfe) {
-					logger.error("Not bean Id or class definition found {}", job.getClassName());
-					throw new Exception("Not bean Id or class definition found " + job.getClassName());
-				}
-			}
-			if (runnableJob != null) {
-				this.listenerDelegate.fireEvent(JOB_PROCESS, this, curQueue, job, null, null, null);
-				if (isThreadNameChangingEnabled()) {
-					renameThread("Processing " + curQueue + " since " + System.currentTimeMillis());
-				}
-				execute(job, curQueue, runnableJob);
-			}
-		} catch (Exception e) {
-			failure(e, job, curQueue);
-		}
-	}
+    private final AtomicBoolean processingJob = new AtomicBoolean(false);
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
-	
-	/**
-	 * Convenient initialization method for the Spring container
-	 */
-	public void init(){
-		logger.info("Start a new thread for SpringWorker");
-		new Thread(this).start();
-	}
-	
-	/**
-	 * Convenient destroy method for the Spring container
-	 */
-	public void destroy(){
-		logger.info("End the SpringWorker thread");
-		end(true);
-	}
+    private final String name;
+
+    @Override
+    public boolean isProcessingJob() {
+        return this.processingJob.get();
+    }
+
+    /**
+     *
+     * @param config used to create a connection to Redis
+     * @param queues the list of queues to poll
+     */
+    public SpringWorker(final Config config, final Collection<String> queues) {
+        super(config, queues, Collections.EMPTY_MAP);
+        this.name = createName();
+    }
+
+    @Override
+    protected void process(final Job job, final String curQueue) {
+        logger.info("Process new Job from queue {}", curQueue);
+        try {
+            Runnable runnableJob = null;
+            if (applicationContext.containsBeanDefinition(job.getClassName())) {//Lookup by bean Id
+                runnableJob = (Runnable) applicationContext.getBean(job.getClassName(), job.getArgs());
+            } else {
+                try {
+                    Class clazz = Class.forName(job.getClassName());//Lookup by Class type
+                    String[] beanNames = applicationContext.getBeanNamesForType(clazz, true, false);
+                    if (applicationContext.containsBeanDefinition(job.getClassName())) {
+                        runnableJob = (Runnable) applicationContext.getBean(beanNames[0], job.getArgs());
+                    } else {
+                        if (beanNames != null && beanNames.length == 1) {
+                            runnableJob = (Runnable) applicationContext.getBean(beanNames[0], job.getArgs());
+                        }
+                    }
+                } catch (ClassNotFoundException cnfe) {
+                    logger.error("Not bean Id or class definition found {}", job.getClassName());
+                    throw new Exception("Not bean Id or class definition found " + job.getClassName());
+                }
+            }
+            if (runnableJob != null) {
+                this.processingJob.set(true);
+                this.listenerDelegate.fireEvent(JOB_PROCESS, this, curQueue, job, null, null, null);
+                this.jedis.set(key(WORKER, this.name), statusMsg(curQueue, job));
+                if (isThreadNameChangingEnabled()) {
+                    renameThread("Processing " + curQueue + " since " + System.currentTimeMillis());
+                }
+                final Object result = execute(job, curQueue, runnableJob);
+                success(job, runnableJob, result, curQueue);
+            }
+        } catch (Exception e) {
+            logger.error("Error while processing the job: " + job.getClassName(), e);
+            failure(e, job, curQueue);
+        } finally {
+            this.jedis.del(key(WORKER, this.name));
+            this.processingJob.set(false);
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * Convenient initialization method for the Spring container
+     */
+    public void init() {
+        logger.info("Start a new thread for SpringWorker");
+        new Thread(this).start();
+    }
+
+    /**
+     * Convenient destroy method for the Spring container
+     */
+    public void destroy() {
+        logger.info("End the SpringWorker thread");
+        end(true);
+    }
 }
